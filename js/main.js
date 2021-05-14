455a816483a414454f111cbd67d12e7f44d04fdc
joyqq <78728463+joyqq@users.noreply.github.com>
2021-05-14 10:04:04 +0800

    111

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("good1").onclick=function(){
+    window.location.href="good1.html"
+}
+
+document.getElementById("good2").onclick=function(){
+    window.location.href="good2.html"
+}
+
+document.getElementById("goodbyh1").onclick=function(){
+    window.location.href="goodbyh1.html"
+}
