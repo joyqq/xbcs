cd79dd4214e81fefcf70f404c49273b7f6c0d370
joyqq <1594554659@qq.com>
2021-05-16 20:52:23 +0800

    1

--- js/main.js
+++ js/main.js
@@ -10,40 +10,44 @@ document.getElementById("good3").onclick=function(){
     window.location.href="good3.html"
 }
 
+document.getElementById("good4").onclick=function(){
+    window.location.href="good4.html"
+}
+
 document.getElementById("goodbyh1").onclick=function(){
     window.location.href="goodbyh1.html"
 }
 
-function getRTime(){ 
-    var EndTime= new Date('2021/05/16 20:00:00'); //截止时间 
-    var NowTime = new Date(); 
-    var t =EndTime.getTime() - NowTime.getTime(); 
+// function getRTime(){ 
+//     var EndTime= new Date('2021/05/16 20:00:00'); //截止时间 
+//     var NowTime = new Date(); 
+//     var t =EndTime.getTime() - NowTime.getTime(); 
     
      
-    // var d=Math.floor(t/1000/60/60/24); 
-    var h=Math.floor(t/1000/60/60); 
-    var m=Math.floor(t/1000/60%60); 
-    var s=Math.floor(t/1000%60); 
+//     // var d=Math.floor(t/1000/60/60/24); 
+//     var h=Math.floor(t/1000/60/60); 
+//     var m=Math.floor(t/1000/60%60); 
+//     var s=Math.floor(t/1000%60); 
      
-    // document.getElementById("t_d").innerHTML = d + "天"; 
-    if (h < 10) {
-        document.getElementById("t_h").innerHTML = "0" + h + "时"; 
-    }
-    else {
-        document.getElementById("t_h").innerHTML = h + "时"; 
-    }
-    if (m < 10) {
-        document.getElementById("t_m").innerHTML = "0" + m + "分"; 
-    }
-    else {
-        document.getElementById("t_m").innerHTML = m + "分"; 
-    }
-    if (s < 10) {
-        document.getElementById("t_s").innerHTML = "0" + s + "秒"; 
-    }
-    else {
-        document.getElementById("t_s").innerHTML = s + "秒"; 
-    }
-} 
+//     // document.getElementById("t_d").innerHTML = d + "天"; 
+//     if (h < 10) {
+//         document.getElementById("t_h").innerHTML = "0" + h + "时"; 
+//     }
+//     else {
+//         document.getElementById("t_h").innerHTML = h + "时"; 
+//     }
+//     if (m < 10) {
+//         document.getElementById("t_m").innerHTML = "0" + m + "分"; 
+//     }
+//     else {
+//         document.getElementById("t_m").innerHTML = m + "分"; 
+//     }
+//     if (s < 10) {
+//         document.getElementById("t_s").innerHTML = "0" + s + "秒"; 
+//     }
+//     else {
+//         document.getElementById("t_s").innerHTML = s + "秒"; 
+//     }
+// } 
     
-setInterval(getRTime,1000); 
+// setInterval(getRTime,1000); 
